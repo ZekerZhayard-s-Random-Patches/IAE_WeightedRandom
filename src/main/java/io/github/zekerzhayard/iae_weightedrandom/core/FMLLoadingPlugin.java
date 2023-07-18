ce22c6155a500571b740de52845675905bed0c55
ZekerZhayard <trees1000@qq.com>
2023-07-18 20:11:55 +0800

    1.0

---
+++ src/main/java/io/github/zekerzhayard/iae_weightedrandom/core/FMLLoadingPlugin.java
@@ -0,0 +1,36 @@
+package io.github.zekerzhayard.iae_weightedrandom.core;
+
+import java.util.Map;
+import javax.annotation.Nullable;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+
+@IFMLLoadingPlugin.Name("IAE_WeightedRandom")
+@IFMLLoadingPlugin.TransformerExclusions("io.github.zekerzhayard.iae_weightedrandom.core.")
+public class FMLLoadingPlugin implements IFMLLoadingPlugin {
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[] { "io.github.zekerzhayard.iae_weightedrandom.core.ClassTransformer" };
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+        RemapUtils.isDevelopmentEnvironment = !(boolean) data.get("runtimeDeobfuscationEnabled");
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
